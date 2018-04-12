de6e0a880771993763a454ce71ed0882be1813ea
Valeria <32876425+ValeriaNARDET@users.noreply.github.com>
2018-04-12 17:13:49 +0300

    Update routerInstance.js

--- common/routerInstance.js
+++ common/routerInstance.js
@@ -11,7 +11,7 @@ const router = new VueRouter({
       path: '/:id',
       name: 'Portfolio',
       component: Portfolio,
-      props: true,
+      props:  ["albums"],
       // children: [,
       //   { 
       //     path: '/:name', 
