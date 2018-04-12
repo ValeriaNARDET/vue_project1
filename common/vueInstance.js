74b0dc7dc50ff0328fbec7cf4a99784d88f5c6fe
Valeria <32876425+ValeriaNARDET@users.noreply.github.com>
2018-04-12 18:21:12 +0300

    Update vueInstance.js

--- common/vueInstance.js
+++ common/vueInstance.js
@@ -17,20 +17,13 @@
   this.$http.get ( this.sourceURL )
       .then ( response => {
          this.rowData = response.body
-    
-
+       for (i=0; i < this.rowData.length; i++) {
+         console.log ( this.rowData[i] )
+       };
       })
       .catch ( err => {
          console.log ( "Ошибка доступа к файлу: " + this.sourceURL)
       })
-  },
-  methods: {
-     consolization: function (){
-       for (i=0; i < this.rowData.length; i++) {
-         console.log ( this.rowData[i] )
-       };
-     }()
   }
-
     router,
   }).$mount('#app')
